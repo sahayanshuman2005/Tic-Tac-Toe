897ba19791632b38d3b530f728223e757041648f
anshuman sahay <167236525+sahayanshuman2005@users.noreply.github.com>
2025-05-31 08:23:05 +0530

    updated win music

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("welcome to tic tac toe");
-let music = new Audio("music.mp3");
+let music = new Audio("oiia music.mp3");
 let audioTurn = new Audio("ting.mp3");
 let gameover = new Audio("gameover.mp3");
 let turn = "X";
